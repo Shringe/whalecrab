0fcb7b988f6a55f61a22ae77fbef20bfac74c8ec
Shringe <dashingkoso@gmail.com>
2025-07-22 17:52:57 -0500

    added the ability to enter fen into TUI

--- src/bin/tui.rs
+++ src/bin/tui.rs
@@ -115,12 +115,91 @@ impl Ascii {
     }
 }
 
+struct Textbox {
+    /// Current value of the input box
+    pub input: String,
+    /// Position of cursor in the editor area.
+    character_index: usize,
+}
+
+impl Textbox {
+    pub fn new() -> Self {
+        Self {
+            input: String::new(),
+            character_index: 0,
+        }
+    }
+
+    pub fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    pub fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    pub fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    /// Returns the byte index based on the character position.
+    ///
+    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
+    /// the byte index based on the index of the character.
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    pub fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+}
+
+#[derive(Debug)]
+enum Focus {
+    Board,
+    Fen,
+}
+
 struct App {
     highlighted_square: Square,
     selected_square: Option<Square>,
     board: Board,
     ascii: Ascii,
     potential_targets: Vec<Square>,
+
+    focus: Focus,
+    fen: Textbox,
     exit: bool,
 }
 
@@ -129,9 +208,12 @@ impl App {
         Self {
             highlighted_square: Square::A1,
             selected_square: None,
-            board: Board::new(),
+            board: Board::default(),
             ascii: Ascii::default(),
             potential_targets: Vec::new(),
+
+            focus: Focus::Board,
+            fen: Textbox::new(),
             exit: false,
         }
     }
@@ -160,6 +242,13 @@ impl App {
     }
 
     fn handle_key_event(&mut self, key_event: event::KeyEvent) {
+        match self.focus {
+            Focus::Board => self.handle_board_key_event(key_event),
+            Focus::Fen => self.handle_fen_key_event(key_event),
+        }
+    }
+
+    fn handle_board_key_event(&mut self, key_event: event::KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
             KeyCode::Char('c') => {
@@ -168,28 +257,26 @@ impl App {
                 }
             }
 
+            KeyCode::Char('m') => self.focus = Focus::Fen,
+
             KeyCode::Left => {
-                let new = self.highlighted_square.left();
-                if new.is_some() {
-                    self.highlighted_square = new.unwrap();
+                if let Some(new) = self.highlighted_square.left() {
+                    self.highlighted_square = new;
                 }
             }
             KeyCode::Down => {
-                let new = self.highlighted_square.down();
-                if new.is_some() {
-                    self.highlighted_square = new.unwrap();
+                if let Some(new) = self.highlighted_square.down() {
+                    self.highlighted_square = new;
                 }
             }
             KeyCode::Up => {
-                let new = self.highlighted_square.up();
-                if new.is_some() {
-                    self.highlighted_square = new.unwrap();
+                if let Some(new) = self.highlighted_square.up() {
+                    self.highlighted_square = new;
                 }
             }
             KeyCode::Right => {
-                let new = self.highlighted_square.right();
-                if new.is_some() {
-                    self.highlighted_square = new.unwrap();
+                if let Some(new) = self.highlighted_square.right() {
+                    self.highlighted_square = new;
                 }
             }
 
@@ -249,6 +336,30 @@ impl App {
         }
     }
 
+    fn handle_fen_key_event(&mut self, key_event: event::KeyEvent) {
+        if key_event.modifiers.contains(KeyModifiers::CONTROL) {
+            match key_event.code {
+                KeyCode::Char('c') => self.exit(),
+                KeyCode::Char('g') => self.fen.input.clear(),
+                _ => {},
+            }
+        } else {
+            match key_event.code {
+                KeyCode::Esc => self.focus = Focus::Board,
+                KeyCode::Left => self.fen.move_cursor_left(),
+                KeyCode::Right => self.fen.move_cursor_right(),
+                KeyCode::Char(c) => self.fen.enter_char(c),
+                KeyCode::Backspace => self.fen.delete_char(),
+                KeyCode::Enter => {
+                    if let Some(valid) = Board::from_fen(&self.fen.input) {
+                        self.board = valid;
+                    }
+                },
+                _ => {},
+            }
+        }
+    }
+         
     fn select(&mut self, sq: Square) {
         self.selected_square = Some(sq);
     }
@@ -272,7 +383,20 @@ impl Widget for &App {
         };
 
         let debug_area = main_layout[0];
-        let grid_area = main_layout[1];
+        let total_grid_area = Layout::vertical([Constraint::Percentage(100), Constraint::Length(3)]).split(main_layout[1]);
+        let grid_area = total_grid_area[0];
+        let fen_area = total_grid_area[1];
+
+        // Fen bar
+        let fen_color = match self.focus {
+            Focus::Board => Color::White,
+            Focus::Fen => Color::Blue,
+        };
+
+        Paragraph::new(self.fen.input.clone())
+            .block(Block::bordered().title("Fen String:"))
+            .fg(fen_color)
+            .render(fen_area, buf);
 
         // Debug info
         let mut debug_text = String::new();
@@ -280,8 +404,9 @@ impl Widget for &App {
             "Screen area:
     width: {}
     height: {}
+    focus: {:?}
 ",
-            area.width, area.height,
+            area.width, area.height, self.focus
         ));
 
         debug_text.push_str(&format!(
@@ -309,7 +434,7 @@ Selected Square info:
         }
 
         Paragraph::new(debug_text)
-            .block(Block::bordered())
+            .block(Block::bordered().title("Debug Info:"))
             .fg(Color::Green)
             .render(debug_area, buf);
 
