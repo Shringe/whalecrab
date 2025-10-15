03d82f944ebc2e3b62b551169badaa5242fa05c6
Shringe <dashingkoso@gmail.com>
2025-10-15 18:03:06 -0500

    fix overlay

--- flake.nix
+++ flake.nix
@@ -10,52 +10,57 @@
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-        version = cargoToml.workspace.package.version;
-      in
-      {
-        packages = {
-          tui = pkgs.rustPlatform.buildRustPackage {
-            inherit version;
-            name = "tui";
-            pname = "whalecrab_tui";
-
-            cargoLock.lockFile = ./Cargo.lock;
-            src = self;
+    let
+      systemOutputs = inputs.flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
           };
 
-          uci = pkgs.rustPlatform.buildRustPackage {
-            inherit version;
-            name = "uci";
-            pname = "whalecrab_uci";
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          version = cargoToml.workspace.package.version;
+        in
+        {
+          packages = {
+            tui = pkgs.rustPlatform.buildRustPackage {
+              inherit version;
+              name = "tui";
+              pname = "whalecrab_tui";
 
-            cargoLock.lockFile = ./Cargo.lock;
-            src = self;
+              cargoLock.lockFile = ./Cargo.lock;
+              src = self;
+            };
+
+            uci = pkgs.rustPlatform.buildRustPackage {
+              inherit version;
+              name = "uci";
+              pname = "whalecrab_uci";
+
+              cargoLock.lockFile = ./Cargo.lock;
+              src = self;
+            };
           };
-        };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            git
-            rustc
-            rustfmt
-            cargo
-            rust-analyzer
-            gnuplot
-          ];
-        };
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              git
+              rustc
+              rustfmt
+              cargo
+              rust-analyzer
+              gnuplot
+            ];
+          };
+        }
+      );
 
-        overlay = final: prev: {
-          whalecrab_tui = "${prev.system}".packages.${prev.system}.tui;
-          whalecrab_uci = "${prev.system}".packages.${prev.system}.uci;
-        };
-      }
-    );
+    in
+    systemOutputs
+    // {
+      overlay = final: prev: {
+        whalecrab_tui = systemOutputs.packages.${prev.system}.tui;
+        whalecrab_uci = systemOutputs.packages.${prev.system}.uci;
+      };
+    };
 }
