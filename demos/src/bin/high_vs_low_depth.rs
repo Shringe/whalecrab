311234abe6c10ca312b41fb7bc2e09c035c9d7fb
Shringe <git@nsaria.com>
2025-10-16 13:32:34 -0500

    added demo

--- demos/src/bin/high_vs_low_depth.rs
+++ demos/src/bin/high_vs_low_depth.rs
@@ -1,23 +1,12 @@
-use rand::Rng;
 use whalecrab_lib::{game::Game, movegen::pieces::piece::Color};
 
 fn main() {
     let mut game = Game::default();
-    let mut rng = rand::rng();
 
     for _ in 0..100 {
         let m = match game.position.turn {
             Color::White => game.get_engine_move_minimax(2),
-            Color::Black => {
-                let moves = game.generate_all_legal_moves();
-                if moves.is_empty() {
-                    None
-                } else {
-                    let chosen_move = rng.random_range(0..moves.len());
-                    let m = moves.get(chosen_move).expect("Chose invalid move");
-                    Some(m.clone())
-                }
-            }
+            Color::Black => game.get_engine_move_minimax(2),
         };
 
         match m {
