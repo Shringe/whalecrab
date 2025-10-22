1dbecf4000a66e2817d319b4b31ba7dc8d99d912
Shringe <git@nsaria.com>
2025-10-22 10:14:14 -0500

    simplified bench output

--- lib/benches/evaluation.rs
+++ lib/benches/evaluation.rs
@@ -1,22 +1,28 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 use whalecrab_lib::game::Game;
 
 fn bench(c: &mut Criterion) {
     let mut game = Game::default();
+    let mut outcome = None;
     c.bench_function("Engine against self", |b| {
         b.iter(|| {
             if let Some(m) = game.get_engine_move_minimax(2) {
                 game.play(&m);
             } else {
+                if outcome.is_none() {
+                    outcome = Some(format!(
+                        "Game ended in {:?}. {} nodes searched.",
+                        game.position.state, game.nodes_seached
+                    ));
+                }
+
                 // Reset the board if no moves to play
-                println!(
-                    "Game ended in {:?}. {} nodes searched.",
-                    game.position.state, game.nodes_seached
-                );
                 game = Game::default();
             }
         })
     });
+
+    println!("{:?}", outcome);
 }
 
 criterion_group! {
