fe71fbea19e3ccc2c1abd216069aef86388b2987
Shringe <git@nsaria.com>
2026-03-09 18:33:05 -0500

    refactored movegen benchmark and added more cases

--- lib/benches/movegen.rs
+++ lib/benches/movegen.rs
@@ -1,45 +1,39 @@
 mod common;
 use criterion::Criterion;
-use whalecrab_lib::game::Game;
+use whalecrab_lib::movegen::pieces::piece::{ALL_PIECE_TYPES, PieceColor};
 
-fn generate_all_psuedo_legal_pawn_moves(game: &mut Game) {
-    for sq in game.white_pawns | game.black_pawns {
-        sq.pawn_psuedo_legal_moves(game);
-    }
-}
+macro_rules! bench_piece_method {
+    ($c:expr, $game:expr, $type:expr, $method:ident) => {
+        let squares = $game.get_pieces(&$type, &PieceColor::White)
+            | $game.get_pieces(&$type, &PieceColor::Black);
 
-fn generate_all_psuedo_legal_rook_moves(game: &mut Game) {
-    for sq in game.white_rooks | game.black_rooks {
-        sq.rook_psuedo_legal_moves(game);
-    }
-}
-
-fn generate_all_psuedo_legal_knight_moves(game: &mut Game) {
-    for sq in game.white_knights | game.black_knights {
-        sq.knight_psuedo_legal_moves(game);
-    }
-}
-
-fn generate_all_psuedo_legal_bishop_moves(game: &mut Game) {
-    for sq in game.white_bishops | game.black_bishops {
-        sq.bishop_psuedo_legal_moves(game);
-    }
-}
-
-fn generate_all_psuedo_legal_queen_moves(game: &mut Game) {
-    for sq in game.white_queens | game.black_queens {
-        sq.queen_psuedo_legal_moves(game);
-    }
-}
-
-fn generate_all_psuedo_legal_king_moves(game: &mut Game) {
-    for sq in game.white_kings | game.black_kings {
-        sq.king_psuedo_legal_moves(game);
-    }
+        $c.bench_function(
+            &format!(
+                "Generate each {:?}.{} on the board",
+                $type,
+                stringify!($method),
+            ),
+            |b| {
+                b.iter(|| {
+                    for sq in squares {
+                        $type.$method(&$game, &sq);
+                    }
+                })
+            },
+        );
+    };
 }
 
 fn bench(c: &mut Criterion) {
     let mut game = common::midgame();
+
+    macro_rules! bench_piece_methods {
+        ($type:expr) => {
+            bench_piece_method!(c, game, $type, psuedo_legal_moves);
+            bench_piece_method!(c, game, $type, psuedo_legal_targets_fast);
+            bench_piece_method!(c, game, $type, legal_moves);
+        };
+    }
 
     c.bench_function("Generate all legal moves", |b| {
         b.iter(|| game.generate_all_legal_moves());
@@ -49,29 +43,9 @@ fn bench(c: &mut Criterion) {
         b.iter(|| game.generate_all_psuedo_legal_moves());
     });
 
-    c.bench_function("Generate all psuedo legal pawn moves", |b| {
-        b.iter(|| generate_all_psuedo_legal_pawn_moves(&mut game))
-    });
-
-    c.bench_function("Generate all psuedo legal rook moves", |b| {
-        b.iter(|| generate_all_psuedo_legal_rook_moves(&mut game))
-    });
-
-    c.bench_function("Generate all psuedo legal knight moves", |b| {
-        b.iter(|| generate_all_psuedo_legal_knight_moves(&mut game))
-    });
-
-    c.bench_function("Generate all psuedo legal bishop moves", |b| {
-        b.iter(|| generate_all_psuedo_legal_bishop_moves(&mut game))
-    });
-
-    c.bench_function("Generate all psuedo legal queen moves", |b| {
-        b.iter(|| generate_all_psuedo_legal_queen_moves(&mut game))
-    });
-
-    c.bench_function("Generate all psuedo legal king moves", |b| {
-        b.iter(|| generate_all_psuedo_legal_king_moves(&mut game))
-    });
+    for p in ALL_PIECE_TYPES {
+        bench_piece_methods!(p);
+    }
 }
 
 setup_criterion!();
