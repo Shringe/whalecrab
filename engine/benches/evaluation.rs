b5598412f3558020585607528e3cdf8e454ae17d
Shringe <git@nsaria.com>
2026-03-11 21:57:47 -0500

    reworked benchmark

--- engine/benches/evaluation.rs
+++ engine/benches/evaluation.rs
@@ -1,30 +1,47 @@
-use criterion::Criterion;
+use criterion::{BenchmarkId, Criterion, Throughput};
 use whalecrab_engine::engine::Engine;
 use whalecrab_lib::game::Game;
 mod common;
 
 fn bench(c: &mut Criterion) {
-    let mut engine = Engine::default();
-    let mut outcome = None;
-    c.bench_function("Engine against self", |b| {
-        b.iter(|| {
-            if let Some(m) = engine.get_engine_move_minimax(2) {
-                engine.game.play(&m);
-            } else {
-                if outcome.is_none() {
-                    outcome = Some(format!(
-                        "Game ended in {:?}. {} nodes searched.",
-                        engine.game.state, engine.game.nodes_seached
-                    ));
+    let mut group = c.benchmark_group("Engine against self");
+    // let mut averages = Vec::new();
+    let mut sample_engine = Engine::default();
+
+    for depth in 0..=0 {
+        let mut nodes_searched = 0;
+        let mut num_completions = 0;
+
+        let _ = sample_engine.get_engine_move_minimax(depth);
+        let sample = sample_engine.game.nodes_seached;
+
+        group.throughput(Throughput::Elements(sample.try_into().unwrap_or(u64::MAX)));
+        println!("{}", sample);
+        group.bench_with_input(BenchmarkId::from_parameter(depth), &depth, |b, &depth| {
+            let mut engine = Engine::default();
+            b.iter(|| {
+                if let Some(m) = engine.get_engine_move_minimax(depth) {
+                    engine.game.play(&m);
+                } else {
+                    // Reset the board if no moves to play
+                    engine.with_new_game(Game::default());
                 }
+            });
 
-                // Reset the board if no moves to play
-                engine.with_new_game(Game::default());
-            }
-        })
-    });
+            nodes_searched += engine.game.nodes_seached;
+            num_completions += 1;
+        });
 
-    println!("{:?}", outcome);
+        let average = nodes_searched / num_completions;
+        println!("Nodes Searched: {}", average);
+    }
+
+    group.finish();
 }
 
-setup_criterion!();
+criterion::criterion_group! {
+    name = benches;
+    config = common::configured_criterion().sample_size(10);
+    targets = bench
+}
+criterion::criterion_main!(benches);
