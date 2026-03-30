04e5caa3479efdc45342ded725da65f029ffa50d
Shringe <git@nsaria.com>
2026-03-30 00:34:42 -0500

    implement basic test

--- uci/src/interface.rs
+++ uci/src/interface.rs
@@ -184,3 +184,28 @@ impl UciInterface {
         (out, UciHandleAction::Continue)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    /// Creates a UciCommand from a formatted string. Unwraps parsing errors.
+    /// ```rust
+    /// let mut uci = UciInterface::default();
+    /// let _ = uci.handle(uci!("uci"));
+    /// let _ = uci.handle(uci!("go movetime {}", 5000));
+    /// ```
+    macro_rules! uci {
+        ($($arg:tt)*) => {{
+            let s = format!($($arg)*);
+            UciCommand::from_str(s.as_str()).expect(format!("Failed to parse uci!({})", s).as_str())
+        }};
+    }
+
+    #[test]
+    fn greeting() {
+        let mut uci = UciInterface::default();
+        let (response, _) = uci.handle(&uci!("uci"));
+        assert!(response.contains(&"uciok".to_string()));
+    }
+}
