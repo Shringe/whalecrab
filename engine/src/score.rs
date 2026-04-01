2ba3a0f6f885ef04d6335000d6204f9d40abad66
Shringe <git@nsaria.com>
2026-03-31 23:54:18 -0500

    add direct i32 operations to score

--- engine/src/score.rs
+++ engine/src/score.rs
@@ -3,34 +3,59 @@ use std::{
     ops::{Add, AddAssign, Sub, SubAssign},
 };
 
+macro_rules! implement_operation {
+    ($trait:ident, $method:ident, $op:tt, Self, assign) => {
+        impl $trait for Score {
+            fn $method(&mut self, other: Self) {
+                self.0 $op other.0;
+            }
+        }
+    };
+    ($trait:ident, $method:ident, $op:tt, $type:ident, assign) => {
+        impl $trait<$type> for Score {
+            fn $method(&mut self, other: $type) {
+                self.0 $op other;
+            }
+        }
+    };
+    ($trait:ident, $method:ident, $op:tt, Self, new) => {
+        impl $trait for Score {
+            type Output = Self;
+            fn $method(self, other: Self) -> Self {
+                Self(self.0 $op other.0)
+            }
+        }
+    };
+    ($trait:ident, $method:ident,  $op:tt, $type:ident, new) => {
+        impl $trait<$type> for Score {
+            type Output = Self;
+            fn $method(self, other: $type) -> Self {
+                Self(self.0 $op other)
+            }
+        }
+    };
+}
+
+macro_rules! implement_operations {
+    (Self) => {
+        implement_operation!(Add, add, +, Self, new);
+        implement_operation!(AddAssign, add_assign, +=, Self, assign);
+        implement_operation!(Sub, sub, -, Self, new);
+        implement_operation!(SubAssign, sub_assign, -=, Self, assign);
+    };
+    ($type:ident) => {
+        implement_operation!(Add, add, +, $type, new);
+        implement_operation!(AddAssign, add_assign, +=, $type, assign);
+        implement_operation!(Sub, sub, -, $type, new);
+        implement_operation!(SubAssign, sub_assign, -=, $type, assign);
+    };
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Default)]
 pub struct Score(i32);
 
-impl Add for Score {
-    type Output = Self;
-    fn add(self, other: Self) -> Self {
-        Score(self.0 + other.0)
-    }
-}
-
-impl Sub for Score {
-    type Output = Self;
-    fn sub(self, other: Self) -> Self {
-        Score(self.0 - other.0)
-    }
-}
-
-impl AddAssign for Score {
-    fn add_assign(&mut self, other: Self) {
-        self.0 += other.0;
-    }
-}
-
-impl SubAssign for Score {
-    fn sub_assign(&mut self, other: Self) {
-        self.0 -= other.0;
-    }
-}
+implement_operations!(Self);
+implement_operations!(i32);
 
 impl fmt::Display for Score {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
