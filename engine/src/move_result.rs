ccd0e0f2a62ac6deac46677ae467f9e35a543698
Shringe <git@nsaria.com>
2026-04-07 23:07:32 -0500

    Add docstring

--- engine/src/move_result.rs
+++ engine/src/move_result.rs
@@ -2,6 +2,7 @@ use whalecrab_lib::movegen::moves::Move;
 
 use crate::score::Score;
 
+/// Provides relevant information about the completed search alongside the best move
 #[derive(Debug, Default)]
 pub struct SearchResult {
     pub best_move: Option<Move>,
