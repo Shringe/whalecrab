7477204802fe37b803c1cb9e862014b8c2ae88e3
Shringe <git@nsaria.com>
2026-04-11 17:59:17 -0500

    Add debug asserts and improve docstrings

--- lib/src/movegen/utils.rs
+++ lib/src/movegen/utils.rs
@@ -1,4 +1,4 @@
-/// During move making you should always the previous pieces before adding new ones so that mailboxes don't collide
+/// During move making you should always remove the previous pieces before adding new ones so that their mailboxes don't collide
 #[macro_export]
 macro_rules! remove_piece {
     ($game:expr, $pieces:expr, $sqbb:expr, $sq:expr) => {
@@ -7,7 +7,7 @@ macro_rules! remove_piece {
     };
 }
 
-/// During move making you should always the previous pieces before adding new ones so that mailboxes don't collide
+/// During move making you should always remove the previous pieces before adding new ones so that their mailboxes don't collide
 #[macro_export]
 macro_rules! add_piece {
     ($game:expr, $pieces:expr, $sqbb:expr, $sq:expr, $piece:expr, $color:expr) => {
@@ -22,6 +22,20 @@ macro_rules! castle {
      $king_from_bb:expr, $king_from:expr, $king_to_bb:expr, $king_to:expr,
      $rook_from_bb:expr, $rook_from:expr, $rook_to_bb:expr, $rook_to:expr,
      $color:expr) => {{
+        debug_assert!(
+            *$kings & $king_from_bb != $crate::bitboard::EMPTY,
+            "King not found on expected castling square {}, white:\n{:?}\nblack:\n{:?}",
+            $king_from,
+            $game.white_kings,
+            $game.black_kings
+        );
+        debug_assert!(
+            *$rooks & $rook_from_bb != $crate::bitboard::EMPTY,
+            "Rook not found on expected castling square {}, white:\n{:?}\nblack:\n{:?}",
+            $rook_from,
+            $game.white_rooks,
+            $game.black_rooks
+        );
         remove_piece!($game, $kings, $king_from_bb, $king_from);
         remove_piece!($game, $rooks, $rook_from_bb, $rook_from);
         add_piece!(
