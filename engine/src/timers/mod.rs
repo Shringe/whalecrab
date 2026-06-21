8153b6485ee2a285209fd8dcb10d3a5295e5d7e0
Shringe <git@nsaria.com>
2026-06-21 14:53:32 -0500

    Finish todo

--- engine/src/timers/mod.rs
+++ engine/src/timers/mod.rs
@@ -4,9 +4,12 @@ pub mod infinite;
 pub mod rdtsc;
 
 /// Returns the high performance `Rdtsc` timer on supported platforms, otherwise returns an `Elapsed` timer
-/// TODO: return an Infinite timer if the Duration == Duration::MAX
 #[macro_export]
 macro_rules! platform_timer {
+    (Duration::MAX) => {{ $crate::timers::infinite::Infinite }};
+    (time::Duration::MAX) => {{ $crate::timers::infinite::Infinite }};
+    (std::time::Duration::MAX) => {{ $crate::timers::infinite::Infinite }};
+    (core::time::Duration::MAX) => {{ $crate::timers::infinite::Infinite }};
     ($duration:expr) => {{
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         {
@@ -23,4 +26,17 @@ macro_rules! platform_timer {
 pub trait MoveTimer {
     /// Checks if the timer has ran out of time
     fn over(&self) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::timers::infinite::Infinite;
+
+    #[test]
+    fn platform_timer_optimizes_known_max_durations_to_infinite() {
+        let _: Infinite = platform_timer!(Duration::MAX);
+        let _: Infinite = platform_timer!(time::Duration::MAX);
+        let _: Infinite = platform_timer!(std::time::Duration::MAX);
+        let _: Infinite = platform_timer!(core::time::Duration::MAX);
+    }
 }
